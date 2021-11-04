64e6b46ed4785697c348508ad7a58a37a91925e8
Hesitater <feelksh0603@gmail.com>
2021-11-04 20:25:14 +0900

    java bean

---
+++ src/sample03javabean/Bean02.java
@@ -0,0 +1,26 @@
+package sample03javabean;
+
+public class Bean02 {
+	
+	private String name;
+	private double age;
+	
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getAge() {
+		return age;
+	}
+
+	public void setAge(double age) {
+		this.age = age;
+	}
+	
+	
+}
