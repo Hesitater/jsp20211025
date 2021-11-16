2b7ee6bf4e0976c476be51bcf3dfc2ad17d11a87
Hesitater <feelksh0603@gmail.com>
2021-11-16 20:47:06 +0900

    todo app

---
+++ src/servlet06/S012AddServlet.java
@@ -0,0 +1,43 @@
+package servlet06;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class S012AddServlet
+ */
+@WebServlet("/servlet06/s012add")
+public class S012AddServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public S012AddServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//forward
+		String path = "/WEB-INF/view/servlet06/s012add.jsp";
+		request.getRequestDispatcher(path).forward(request, response);
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
