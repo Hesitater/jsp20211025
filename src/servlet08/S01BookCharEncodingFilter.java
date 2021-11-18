bc8d5e6c655d51e9e626a96e8eb79dea73343daa
Hesitater <feelksh0603@gmail.com>
2021-11-18 18:55:01 +0900

    filter

--- src/servlet08/S01BookCharEncodingFilter.java
+++ src/servlet08/S01BookCharEncodingFilter.java
@@ -35,7 +35,8 @@ public class S01BookCharEncodingFilter implements Filter {
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
 		// TODO Auto-generated method stub
 		// place your code here
-		System.out.println("북차엔코딩필터!!");
+		System.out.println("북차엔코딩필터!! servlet08 S01BookChar");
+		
 //		request.setCharacterEncoding("utf-8");
 		// pass the request along the filter chain
 		chain.doFilter(request, response);
