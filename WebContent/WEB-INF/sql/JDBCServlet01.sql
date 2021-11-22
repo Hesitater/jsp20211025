c85cd5f6ed8cb2fc1a2c4ecb4ae98eb82b498762
Hesitater <feelksh0603@gmail.com>
2021-11-22 19:28:56 +0900

    sql

---
+++ WebContent/WEB-INF/sql/JDBCServlet01.sql
@@ -0,0 +1,9 @@
+SELECT 333;
+
+SELECT CustomerName FROM Customers WHERE CustomerID = 1;
+ 
+SELECT LastName FROM Employees WHERE EmployeeID = 1;
+
+
+SELECT * FROM OrderDetails;
+SELECT * FROM Employees;
