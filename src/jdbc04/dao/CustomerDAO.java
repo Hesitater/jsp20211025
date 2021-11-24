b695161e62e4dafefced0d52d0b748e2421b70bf
Hesitater <feelksh0603@gmail.com>
2021-11-24 18:26:47 +0900

    jdbc insert

--- src/jdbc04/dao/CustomerDAO.java
+++ src/jdbc04/dao/CustomerDAO.java
@@ -13,37 +13,35 @@ public class CustomerDAO {
 
 	public List<String> getCountryList(Connection con) {
 		List<String> list = new ArrayList<String>();
-		
+
 		String sql = "SELECT DISTINCT country FROM Customers ORDER BY country";
-		
-		try ( Statement stmt = con.createStatement();
-				ResultSet rs = stmt.executeQuery(sql);) {
-			
-			while( rs.next()) {
+
+		try (Statement stmt = con.createStatement(); ResultSet rs = stmt.executeQuery(sql);) {
+
+			while (rs.next()) {
 				list.add(rs.getString(1));
 			}
-			
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+
 		return list;
 	}
-
 
 	public List<Customer> getCustomerListByCountry(Connection con, String country) {
 		List<Customer> list = new ArrayList<Customer>();
 		String sql = "SELECT CustomerID, CustomerName, ContactName, Address, City, PostalCode, Country "
 				+ " FROM Customers WHERE Country = ?";
-		
-		try ( PreparedStatement pstmt = con.prepareStatement(sql) ) {
-			
+
+		try (PreparedStatement pstmt = con.prepareStatement(sql)) {
+
 			pstmt.setString(1, country);
-			
+
 			try (ResultSet rs = pstmt.executeQuery()) {
 				while (rs.next()) {
 					Customer cus = new Customer();
-					
+
 					int i = 1;
 					cus.setCustomerID(rs.getInt(i++));
 					cus.setCustomerName(rs.getString(i++));
@@ -52,23 +50,87 @@ public class CustomerDAO {
 					cus.setCity(rs.getString(i++));
 					cus.setPostalCode(rs.getString(i++));
 					cus.setCountry(rs.getString(i));
-					
+
 					list.add(cus);
-				}	
+				}
 			}
-			
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return list;
-	
+
 	}
 
-	
-}	
-	
-	
-	
-	
-	
-	
+	public boolean insert(Connection con, Customer customer) {
+		String sql = "INSERT INTO Customers (CustomerName, ContactName, Address, City, PostalCode, Country) "
+				+ "   VALUES (?, ?, ?, ?, ?, ?)";
+
+		int rowCount = 0;
+		try (PreparedStatement pstmt = con.prepareStatement(sql)) {
+
+			// ? 채우기
+			pstmt.setString(1, customer.getCustomerName());
+			pstmt.setString(2, customer.getContactName());
+			pstmt.setString(3, customer.getAddress());
+			pstmt.setString(4, customer.getCity());
+			pstmt.setString(5, customer.getPostalCode());
+			pstmt.setString(6, customer.getCountry());
+
+//			pstmt.executeUpdate();
+			rowCount = pstmt.executeUpdate();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return rowCount == 1;
+
+	}
+
+	public boolean update(Connection con, Customer customer) {
+		String sql = "UPDATE Customers " + 
+				" SET CustomerName = ?, " + 
+				"	ContactName= ?, " + 
+				"    Address = ?, " + 
+				"    City = ?, " + 
+				"    PostalCode = ?, " + 
+				"    Country = ? " + 
+				"  WHERE " + 
+				"	CustomerID = ? ";
+		
+		int rowCount = 0;
+		
+		try(PreparedStatement pstmt = con.prepareStatement(sql)) {
+			int i = 1;
+			pstmt.setString(i++, customer.getCustomerName());
+			pstmt.setString(i++, customer.getContactName());
+			pstmt.setString(i++, customer.getAddress());
+			pstmt.setString(i++, customer.getCity());
+			pstmt.setString(i++, customer.getPostalCode());
+			pstmt.setString(i++, customer.getCountry());
+			pstmt.setInt(i++, customer.getCustomerID());
+			
+			rowCount = pstmt.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		
+		return rowCount == 1;
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
