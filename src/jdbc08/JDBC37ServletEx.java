7e01229b4bca7725177b2fe14bdf0ecb285eebe6
Hesitater <feelksh0603@gmail.com>
2021-11-27 12:42:35 +0900

    insert ex

---
+++ src/jdbc08/JDBC37ServletEx.java
@@ -0,0 +1,77 @@
+package jdbc08;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.time.LocalDate;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+
+import jdbc08.bean.Bean11;
+import jdbc08.dao.MyTable11Dao;
+
+/**
+ * Servlet implementation class JDBC37ServletEx
+ */
+@WebServlet("/JDBC37ServletEx")
+public class JDBC37ServletEx extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public JDBC37ServletEx() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String path = "/WEB-INF/view/jdbc08/v37Ex.jsp";
+		request.getRequestDispatcher(path).forward(request, response);
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+
+		// 0. 사전 작업
+		ServletContext application = request.getServletContext();
+		DataSource ds= (DataSource) application.getAttribute("dbpool");
+		MyTable11Dao dao = new MyTable11Dao();
+		
+		// 2. request 분석, 가공
+		Bean11 bean = new Bean11();
+		bean.setName(request.getParameter("name"));
+		bean.setAge(Integer.parseInt(request.getParameter("age")));
+		bean.setBirth(LocalDate.parse(request.getParameter("birth")));
+		
+		
+		// 3. business logic( 주로 db작업)
+		try ( Connection con = ds.getConnection()) {
+			dao.insert(con, bean);			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		// 4. setattribute
+		
+		
+		// 5. forward/ redirect
+		String location = request.getContextPath() + "/JDBC38ServletEx";
+		response.sendRedirect(location);
+		
+		
+	}
+
+}
