743871beeaf6fb1630d272213b387c0336019da7
Hesitater <feelksh0603@gmail.com>
2021-11-30 08:55:19 +0900

    union

---
+++ WebContent/WEB-INF/sql/26union.sql
@@ -0,0 +1,52 @@
+USE test;
+
+SELECT * FROM mytable27Customer;	-- 3,4,5,6
+SELECT * FROM mytable28Tel;     -- 1,3,4 
+
+-- 한줄로 나열하고 싶을때..?
+-- 컬럼의 수가 같은 조회 결과의 합집합
+SELECT firstName FROM mytable27Customer
+
+UNION
+ 
+SELECT lastName FROM mytable27Customer;
+
+
+INSERT INTO mytable27Customer (firstName, lastName) VALUES ('ji', 'sj')    ; 
+
+
+-- 중복된 값도 조회
+SELECT firstName FROM mytable27Customer
+
+UNION ALL
+ 
+SELECT lastName FROM mytable27Customer;
+
+
+
+-- uniton 사용해서 full outer join
+
+SELECT * FROM mytable27Customer;	-- 3,4,5,6,7
+SELECT * FROM mytable28Tel;     -- 1,3,4 
+
+SELECT * FROM mytable27Customer c LEFT JOIN mytable28Tel t On c.id= t.customerId
+
+UNION
+
+SELECT * FROM mytable27Customer c RIGHT JOIN mytable28Tel t On c.id= t.customerId;
+
+
+
+-- 연습 : 고객명과, 직원의 LastName을 하나의 컬럼으로 조회
+SELECT * FROM Customers;
+SELECT * FROM Employees; 
+
+-- 연습 : 고객명과, 직원의 LastName을 하나의 컬럼으로 조회
+SELECT CustomerName FROM Customers
+UNION
+SELECT LastName FROM Employees
+ORDER BY 1
+;
+
+
+
