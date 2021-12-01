949a68b5fb1e030e24e9992656f78fca8df23dab
Hesitater <feelksh0603@gmail.com>
2021-12-01 20:31:21 +0900

    between

---
+++ WebContent/WEB-INF/sql/37between.sql
@@ -0,0 +1,33 @@
+USE test;
+
+SELECT * FROM Products;
+
+SELECT * FROM Products
+WHERE Price >=10;
+
+SELECT * FROM Products
+WHERE Price <=20;
+
+SELECT * FROM Products
+WHERE Price >= 10 AND Price <= 20;
+
+SELECT * FROM Products
+WHERE Price BETWEEN 10 AND 20;
+
+SELECT * FROM Products
+WHERE ProductName BETWEEN 'C' AND 'F';
+
+SELECT * FROM Products
+WHERE ProductName BETWEEN 'C' ANd 'G';
+
+SELECT * FROm Products;
+
+SELECT * FROM Employees ORDER BY BirthDate;
+SELECT * FROm Employees WHERE BirthDate BETWEEN '1958-01-01' AND '1958-12-31';
+
+DESC Employees;
+
+
+
+
+
