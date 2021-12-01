7c7faab68ff5d4d0b5bc9ef3420eaabff46c0529
Hesitater <feelksh0603@gmail.com>
2021-12-01 20:30:38 +0900

    union

--- WebContent/WEB-INF/sql/26union.sql
+++ WebContent/WEB-INF/sql/26union.sql
@@ -12,7 +12,7 @@ UNION
 SELECT lastName FROM mytable27Customer;
 
 
-INSERT INTO mytable27Customer (firstName, lastName) VALUES ('ji', 'sj')    ; 
+INSERT INTO mytable27Customer (firstName, lastName) VALUES ('ji', 'sj'); 
 
 
 -- 중복된 값도 조회
@@ -24,7 +24,7 @@ SELECT lastName FROM mytable27Customer;
 
 
 
--- uniton 사용해서 full outer join
+-- union 사용해서 full outer join
 
 SELECT * FROM mytable27Customer;	-- 3,4,5,6,7
 SELECT * FROM mytable28Tel;     -- 1,3,4 
@@ -35,11 +35,6 @@ UNION
 
 SELECT * FROM mytable27Customer c RIGHT JOIN mytable28Tel t On c.id= t.customerId;
 
-
-
--- 연습 : 고객명과, 직원의 LastName을 하나의 컬럼으로 조회
-SELECT * FROM Customers;
-SELECT * FROM Employees; 
 
 -- 연습 : 고객명과, 직원의 LastName을 하나의 컬럼으로 조회
 SELECT CustomerName FROM Customers
